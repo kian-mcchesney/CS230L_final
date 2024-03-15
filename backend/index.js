b0d81349a334311cb9f874e013b6275453d29ae8
kian-mcchesney <kianmcchesney@gmail.com>
2024-03-15 12:46:39 -0400

    finally getting books data

--- backend/index.js
+++ backend/index.js
@@ -3,25 +3,37 @@ import mysql from "mysql"
 
 const app = express()
 const db  = mysql.createConnection({
-    host: "local",
+    host: "localhost",
     user: "root",
     password: "password123",
     database: "test"
 })
 //
+// Connect to MySQL database
+db.connect((err) => {
+    if (err) {
+        console.error("Error connecting to database:", err);
+        return;
+    }
+    console.log("Connected to MySQL database!");
+});
+
+
 app.get("/", (req,res) => {
     res.json("hello this is the backend.")
 
 })
 
-app.get("/books", (req,res)=>{
-    const q = "SELECT * FROM books"
-    db.query(q,(err,data)=> {
-        if(err) return res.json(err)
-        return res.json(data)
-    })
-
-})
+app.get("/books", (req, res) => {
+    const query = "SELECT * FROM books";
+    db.query(query, (err, data) => {
+        if (err) {
+            console.error("Error executing MySQL query:", err);
+            return res.status(500).json({ error: "Internal server error" });
+        }
+        return res.json(data);
+    });
+});
 
 app.listen(8800,() => {
     console.log("Connected to backend!");
