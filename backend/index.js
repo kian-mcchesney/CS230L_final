71881fbe54d1409f3c2dce9f60be835791c0bf55
kian-mcchesney <kianmcchesney@gmail.com>
2024-03-15 11:18:07 -0400

    create database connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from "express"
+import mysql from "mysql"
 
 const app = express()
+const db  = mysql.createConnection({
+    host: "local",
+    user: "root",
+    password: "password",
+    database: "test",
+})
+//
+app.get("/", (req,res) => {
+    res.json("hello this is the backend")
 
+})
 
-app.listen(8800, () => {
-    console.log("Connected to backend.");
-});
+//getting an error for EADDRINUSE so used this code to find available port
+const server= app.listen(0,() => {
+    const port = server.address().port;
+    console.log("Connected to backend!");
+})
+
