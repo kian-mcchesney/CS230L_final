68128967fe9b901acdbff445204a319c8fa5ec52
kian-mcchesney <kianmcchesney@gmail.com>
2024-03-19 15:38:29 -0400

    Can add books!!

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Books from './pages/Books';
 import Add from './pages/Add';
 import Update from './pages/Update';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import navbar from './pages/navigationbar';
 
 
 
@@ -18,6 +19,7 @@ function App() {
     <div className="App">
     
       <BrowserRouter>
+    
         <Routes>
           <Route path="/" element={<Books/>} />
           <Route path="/add" element={<Add/>} />
--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import {useState} from "react";
+import {Link, useNavigate} from "react-router-dom"
+import axios from 'axios';
 
 const Add = ()=> {
+    const [book,setBook]=useState({
+        title:"",
+        description:"",
+        price:null,
+        cover:"",
+
+    });
+
+
+const handleChange=(e)=>{
+    setBook((prev) => ({...prev,[e.target.name]: e.target.value}));
+}
+const handleClick = async e =>{
+    e.prevetDefault();
+    try {
+        await axios.post("http://localhost:800/books",book);
+        navigate("/")
+    } catch (err) {
+        console.log(err);
+        setError(true)
+        
+    }
+}
+const [error,setError] = useState(false)
+const navigate = useNavigate();
+
+console.log(book);
 
     return(
-        <div>
-            Add
+        <div className='form'>
+
+            <h1>Add New Book</h1>
+            <input type="text" placeholder='title' onChange={handleChange} name="title"></input>
+            <input type="text" placeholder='description' onChange={handleChange} name="description"></input>
+            <input type="number" placeholder='price' onChange={handleChange} name = "price"></input>
+            <input type="text" placeholder='cover' onChange={handleChange}name="cover"></input>
+
+        <button onClick={handleClick}>Add</button>
         </div>
     )
 }
